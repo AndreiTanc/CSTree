964de58b8b13a75f8220db111318ca9f8551bfff
Horea Oros <horea.oros@gmail.com>
2017-11-13 16:10:00 +0200

    add tree project

---
+++ Tree/Tree/BST.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Tree
+{
+    internal class Node
+    {
+        int key;
+        int count;
+        Node left, right;
+
+        public Node(int key, int count)
+        {
+            this.key = key;
+            this.count = count;
+            left = right = null;
+        }
+        public int Key
+        {
+            get
+            {
+                return key;
+            }
+        }
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+             set
+             {
+                count = value;
+             }
+        }
+
+        public Node Left
+        {
+            get { return left; }
+            set { left = value; }
+        }
+
+
+        public Node Right
+        {
+            get { return right; }
+            set { right = value; }
+        }
+    }
+    internal class BST
+    {
+        Node root;
+        public BST()
+        {
+            Console.WriteLine("Am creat un BST");
+            root = null;
+
+        }
+        public void Add(int key)
+        {
+            bool inserted = false;
+
+            Node p = root;
+            if (root == null)
+            {
+                root = new Node(key, 1);
+                inserted = true;
+                Debug.WriteLine("Am creat nodul radacina");
+            }
+                
+            while (!inserted)
+            {
+                if (p.Key == key)
+                {
+                    p.Count = p.Count + 1;
+                    inserted = true;
+                }
+                    
+                else if(p.Key > key)
+                 {
+                    if (p.Left == null)
+                    {
+                        // creare nou nod
+                        Node q;
+                        q = new Node(key, 1);
+                        p.Left = q;
+                        inserted = true;
+                    }
+                    else
+                        p = p.Left;
+                        
+                 }
+                else
+                {
+                    if (p.Right == null)
+                    {
+                        Node q;
+                        q = new Node(key, 1);
+                        p.Right = q;
+                        inserted = true;
+                    }
+                    else
+                        p = p.Right;
+                }
+            }
+        }
+
+        internal void Print()
+        {
+            Print(root);
+        }
+
+        internal bool Search(int val)
+        {
+            Node p = root;
+
+            while (p != null)
+            {
+                if (p.Key == val)
+                    return true;
+                else
+                    if (p.Key < val)
+                        p = p.Right;
+                    else
+                        p = p.Left;
+
+            }
+            return false;
+        }
+
+        private void Print(Node p)
+        {
+            if(p != null)
+            {
+                Print(p.Left);
+                Console.WriteLine("{0}, {1}", p.Key, p.Count);
+                Print(p.Right);
+            }
+        }
+    }
+}
---
+++ Tree/Tree/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BST b = new BST();
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < 20; i++)
+            {
+                b.Add(rnd.Next(0, 100));
+            }
+
+            b.Print();
+
+            int val = rnd.Next(0, 100);
+            Console.WriteLine("{0} in tree: {1}", val, b.Search(val));
+
+
+            Dictionary<int, int> d = new Dictionary<int, int>();
+
+
+            Console.ReadKey();
+        }
+    }
+}
